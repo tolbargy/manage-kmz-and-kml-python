998f23846d1973797dc45a4368b76dc19e42121c
Jorge Armando Toledo - Ceiba Software <jorge.toledo@ceiba.com.co>
2021-06-03 01:19:28 -0500

    ajuste pykml

--- run-pykml.py
+++ run-pykml.py
@@ -3,6 +3,10 @@ from pykml import parser
 from lxml import etree, objectify
 import logging
 import zipfile
+import os
+import xml.etree.ElementTree as ET
+import sys
+sys.setrecursionlimit(1500)
 
 KML = objectify.ElementMaker(annotate=False)
 logging.getLogger().setLevel(logging.INFO)
@@ -14,29 +18,18 @@ default_namespace = "http://www.opengis.net/kml/2.2"
 
 def main():
     modificar_kml_y_volver_comprimir()
+    eliminar_antiguo_kmz_y_renombrar_nuevo_kmz()
 
-    with open('resources/solo-punto.kml','r') as f:
-        punto_tree = parser.parse(f)
-        punto_root = punto_tree.getroot()
-
-
-    document_punto = KML.Document("")
-    punto_root.append(document_punto)
-
-    document_ruta = ruta_root.Document
-    document_punto.append(document_ruta)
-
-    with open('resources/output.kml', 'w') as output:
-        resultado = etree.tostring(punto_tree,encoding='UTF-8', method='xml',pretty_print=True,xml_declaration=True).decode()
-        output.write(resultado)                
-
+def eliminar_antiguo_kmz_y_renombrar_nuevo_kmz():
+    os.remove(path_kmz_original)
+    os.rename(path_kmz_tmp,path_kmz_original)
 
 def modificar_kml_y_volver_comprimir():
    with zipfile.ZipFile(path_kmz_original) as inzip, zipfile.ZipFile(path_kmz_tmp, "w") as outzip:
     for inzipinfo in inzip.infolist():
         with inzip.open(inzipinfo) as infile:
             if inzipinfo.filename == name_file_kml:                
-                content = infile.read().decode()
+                content = infile.read()
                 xml_final = procesar_agregacion_rutas(content)                                               
                 outzip.writestr(inzipinfo.filename, xml_final)
             else:
@@ -47,18 +40,15 @@ def procesar_agregacion_rutas(content):
     element_document = puntos_tree.Document
     agregarDocumentRutaHaciaDocumentPunto(element_document)               
 
-    etree.register_namespace('',default_namespace)
-    stream = BytesIO()
-    etree.ElementTree(puntos_tree).write(stream,encoding='UTF-8', method='xml',xml_declaration=True)
-    xml_final = stream.getvalue().decode()
+    xml_final= etree.tostring(puntos_tree,pretty_print=True,xml_declaration=True, encoding='UTF-8')
     return xml_final
 
 def agregarDocumentRutaHaciaDocumentPunto(element_document_punto):    
     with open(path_rutas,'r') as f:
-        ruta_tree = parser.parse(f)
+        ruta_tree = parser.parse(f)        
         ruta_root = ruta_tree.getroot()
 
-        elements_document_ruta = ruta_root.Document
+        elements_document_ruta = ruta_root.Document.getchildren()
         for element in elements_document_ruta:
             logging.info(f"Se agregó elemento {element}")
             element_document_punto.append(element)
