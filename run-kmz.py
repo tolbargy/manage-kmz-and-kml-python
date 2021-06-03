f9cc028821f88d103a703955583dd5ab80475fbf
Jorge Armando Toledo - Ceiba Software <jorge.toledo@ceiba.com.co>
2021-06-02 20:11:20 -0500

    ajuste script kmz

--- run-kmz.py
+++ run-kmz.py
@@ -1,6 +1,7 @@
 import xml.etree.ElementTree as ET
 import logging
 import zipfile
+import os
 
 logging.getLogger().setLevel(logging.INFO)
 path_kmz_original = 'resources/final.kmz'
@@ -11,6 +12,14 @@ default_namespace = "http://www.opengis.net/kml/2.2"
 ns = {'default': default_namespace}
 
 def main():      
+    modificar_kml_y_volver_comprimir()
+    eliminar_antiguo_kmz_y_renombrar_nuevo_kmz()
+
+def eliminar_antiguo_kmz_y_renombrar_nuevo_kmz():
+    os.remove(path_kmz_original)
+    os.rename(path_kmz_tmp,path_kmz_original)
+
+def modificar_kml_y_volver_comprimir():
    with zipfile.ZipFile(path_kmz_original) as inzip, zipfile.ZipFile(path_kmz_tmp, "w") as outzip:
     for inzipinfo in inzip.infolist():
         with inzip.open(inzipinfo) as infile:
@@ -20,7 +29,6 @@ def main():
                 outzip.writestr(inzipinfo.filename, xml_final)
             else:
                outzip.writestr(inzipinfo.filename, infile.read())
-
 
 def procesar_agregacion_rutas(content):
     puntos_tree = ET.fromstring(content)
